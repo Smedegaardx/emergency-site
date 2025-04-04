8d071af179d73b2c45dc471f2ff2564d0065bd0a
Anthon <anan0013@stud.kea.dk>
2025-04-04 12:46:56 +0200

    Lidt brødtekst ændret

--- js/main.js
+++ js/main.js
@@ -24,18 +24,19 @@ function removeBorder() {
 function sunText() {
   console.log(this);
   document.querySelector("#overskrift").innerHTML = "Solen";
-  document.querySelector("#breadtext").innerHTML = "Vi tester lige 123";
-  document.querySelector("#efficiency").innerHTML = "Vi tester lige 123";
-  document.querySelector("#requirement").innerHTML = "Vi tester lige 123";
+  document.querySelector("#breadtext").innerHTML = "Solen er den største fare for menneskeheden. Det vides ikke hvorfor eller hvordan solen pludselig er blevet fjendltig, men staten arbejder <span> stærkt </span> på at finde en løsning";
+  document.querySelector("#efficiency").innerHTML = "Indsæt liste over hvorfor solen mugligvis er fjendtlig";
+  document.querySelector("#requirement").innerHTML = "Indsæt propaganda for hvad staten gør for at stoppe det";
   document.querySelector("#Pyramid").classList.remove("pyramidhide1");
+  document.querySelector("#requirement").classList.remove("hide");
 }
 
 function beamText() {
   console.log(this);
   document.querySelector("#overskrift").innerHTML = "Konsekvenser";
-  document.querySelector("#breadtext").innerHTML = "Vi tester lige 123";
-  document.querySelector("#efficiency").innerHTML = "Vi tester lige 123";
-  document.querySelector("#requirement").innerHTML = "Vi tester lige 123";
+  document.querySelector("#breadtext").innerHTML = "Solen's strålingsniveau er <span> STÆRKT </span> forøget. Det kan have store konsekvenser for en menneskelig krop. Herunder kan læses nærmere";
+  document.querySelector("#efficiency").innerHTML = "Indsæt her ting solen kan gøre ved kroppen";
+  document.querySelector("#requirement").classList.add("hide");
   document.querySelector("#Pyramid").classList.remove("pyramidhide2");
 }
 
@@ -45,15 +46,17 @@ function pyramidText() {
   document.querySelector("#breadtext").innerHTML = "Vi tester lige 123";
   document.querySelector("#efficiency").innerHTML = "Vi tester lige 123";
   document.querySelector("#requirement").innerHTML = "Vi tester lige 123";
+  document.querySelector("#requirement").classList.remove("hide");
   //   document.querySelector("#requirement").classList.remove("hide");
 }
 
 function houseText() {
   console.log(this);
   document.querySelector("#overskrift").innerHTML = "Beskyttelse";
-  document.querySelector("#breadtext").innerHTML = "Vi tester lige 123";
+  document.querySelector("#breadtext").innerHTML = "";
   document.querySelector("#efficiency").innerHTML = "Vi tester lige 123";
   document.querySelector("#requirement").innerHTML = "Vi tester lige 123";
   document.querySelector("#Pyramid").classList.remove("pyramidhide3");
+  document.querySelector("#requirement").classList.remove("hide");
   //   document.querySelector("#requirement").classList.add("hide");
 }
