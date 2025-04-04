cfd0abcc8a8b571d4ab4d15ae8f1f86b30b0ec3f
Anthon <anan0013@stud.kea.dk>
2025-04-04 13:45:21 +0200

    Rød farve tilføjet til den ene infobox

--- js/main.js
+++ js/main.js
@@ -29,6 +29,7 @@ function sunText() {
   document.querySelector("#requirement").innerHTML = "Indsæt propaganda for hvad staten gør for at stoppe det";
   document.querySelector("#Pyramid").classList.remove("pyramidhide1");
   document.querySelector("#requirement").classList.remove("hide");
+  document.querySelector("#efficiency").classList.remove("red");
 }
 
 function beamText() {
@@ -38,6 +39,7 @@ function beamText() {
   document.querySelector("#efficiency").innerHTML = "Indsæt her ting solen kan gøre ved kroppen";
   document.querySelector("#requirement").classList.add("hide");
   document.querySelector("#Pyramid").classList.remove("pyramidhide2");
+  document.querySelector("#efficiency").classList.remove("red");
 }
 
 function pyramidText() {
@@ -47,16 +49,18 @@ function pyramidText() {
   document.querySelector("#efficiency").innerHTML = "Vi tester lige 123";
   document.querySelector("#requirement").innerHTML = "Vi tester lige 123";
   document.querySelector("#requirement").classList.remove("hide");
+  document.querySelector("#efficiency").classList.remove("red");
   //   document.querySelector("#requirement").classList.remove("hide");
 }
 
 function houseText() {
   console.log(this);
   document.querySelector("#overskrift").innerHTML = "Beskyttelse";
-  document.querySelector("#breadtext").innerHTML = "";
+  document.querySelector("#breadtext").innerHTML = "Det er vigtigt at man sørge for at beskytte sig imod solen. Her kan du se lidt forskellige do's and dont's";
   document.querySelector("#efficiency").innerHTML = "Vi tester lige 123";
   document.querySelector("#requirement").innerHTML = "Vi tester lige 123";
   document.querySelector("#Pyramid").classList.remove("pyramidhide3");
   document.querySelector("#requirement").classList.remove("hide");
+  document.querySelector("#efficiency").classList.add("red");
   //   document.querySelector("#requirement").classList.add("hide");
 }
